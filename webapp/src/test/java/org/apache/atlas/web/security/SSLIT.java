d4183ed10ddeef9f39c3e62ab05dbf2f526589c3
Shwetha GS <sshivalingamurthy@hortonworks.com>
2015-07-31 09:22:55 +0530

    ATLAS-86 Jenkins build failing as of build #41 (shwethags)

--- webapp/src/test/java/org/apache/atlas/web/security/SSLIT.java
+++ webapp/src/test/java/org/apache/atlas/web/security/SSLIT.java
@@ -16,53 +16,33 @@
  * limitations under the License.
  */
 
-package org.apache.atlas.hive.hook;
+package org.apache.atlas.web.security;
 
 import org.apache.atlas.AtlasClient;
 import org.apache.atlas.AtlasException;
-import org.apache.atlas.hive.bridge.HiveMetaStoreBridge;
-import org.apache.atlas.hive.model.HiveDataTypes;
-import org.apache.atlas.security.SecurityProperties;
+import org.apache.atlas.web.TestUtils;
 import org.apache.atlas.web.service.SecureEmbeddedServer;
 import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.commons.lang.RandomStringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hive.conf.HiveConf;
-import org.apache.hadoop.hive.ql.Driver;
-import org.apache.hadoop.hive.ql.session.SessionState;
 import org.apache.hadoop.security.alias.CredentialProvider;
 import org.apache.hadoop.security.alias.CredentialProviderFactory;
 import org.apache.hadoop.security.alias.JavaKeyStoreProvider;
-import org.apache.hadoop.security.ssl.SSLFactory;
-import org.apache.hadoop.security.ssl.SSLHostnameVerifier;
-import org.codehaus.jettison.json.JSONArray;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.URL;
 import java.nio.file.Files;
 
-import static org.apache.atlas.security.SecurityProperties.CERT_STORES_CREDENTIAL_PROVIDER_PATH;
-import static org.apache.atlas.security.SecurityProperties.KEYSTORE_FILE_KEY;
 import static org.apache.atlas.security.SecurityProperties.KEYSTORE_PASSWORD_KEY;
 import static org.apache.atlas.security.SecurityProperties.SERVER_CERT_PASSWORD_KEY;
-import static org.apache.atlas.security.SecurityProperties.TLS_ENABLED;
-import static org.apache.atlas.security.SecurityProperties.TRUSTSTORE_FILE_KEY;
 import static org.apache.atlas.security.SecurityProperties.TRUSTSTORE_PASSWORD_KEY;
 
-public class SSLHiveHookIT {
-    private static final String DGI_URL = "https://localhost:21443/";
-    private Driver driver;
+public class SSLIT extends BaseSSLAndKerberosTest {
     private AtlasClient dgiCLient;
-    private SessionState ss;
     private Path jksPath;
     private String providerUrl;
     private TestSecureEmbeddedServer secureEmbeddedServer;
@@ -78,49 +58,22 @@ public class SSLHiveHookIT {
         }
 
         @Override
-        public PropertiesConfiguration getConfiguration() {
+        public org.apache.commons.configuration.Configuration getConfiguration() {
             return super.getConfiguration();
         }
     }
 
     @BeforeClass
     public void setUp() throws Exception {
-        //Set-up hive session
-        HiveConf conf = getHiveConf();
-        HiveHook.hiveConf = conf;
-
-        driver = new Driver(conf);
-        ss = new SessionState(conf, System.getProperty("user.name"));
-        ss = SessionState.start(ss);
-        SessionState.setCurrentSessionState(ss);
-
         jksPath = new Path(Files.createTempDirectory("tempproviders").toString(), "test.jks");
         providerUrl = JavaKeyStoreProvider.SCHEME_NAME + "://file" + jksPath.toUri();
 
-        String persistDir = null;
-        URL resource = SSLHiveHookIT.class.getResource("/");
-        if (resource != null) {
-            persistDir = resource.toURI().getPath();
-        }
-        // delete prior ssl-client.xml file
-        resource = SSLHiveHookIT.class.getResource("/" + SecurityProperties.SSL_CLIENT_PROPERTIES);
-        if (resource != null) {
-            File sslClientFile = new File(persistDir, SecurityProperties.SSL_CLIENT_PROPERTIES);
-            if (sslClientFile != null && sslClientFile.exists()) {
-                sslClientFile.delete();
-            }
-        }
+        String persistDir = TestUtils.getTempDirectory();
+
         setupCredentials();
 
-        final PropertiesConfiguration configuration = new PropertiesConfiguration();
-        configuration.setProperty(TLS_ENABLED, true);
-        configuration.setProperty(TRUSTSTORE_FILE_KEY, "../../webapp/target/atlas.keystore");
-        configuration.setProperty(KEYSTORE_FILE_KEY, "../../webapp/target/atlas.keystore");
-        configuration.setProperty(CERT_STORES_CREDENTIAL_PROVIDER_PATH, providerUrl);
-        configuration.setProperty(SSLFactory.SSL_HOSTNAME_VERIFIER_KEY,
-                SSLHostnameVerifier.DEFAULT_AND_LOCALHOST.toString());
-
-        configuration.save(new FileWriter(persistDir + File.separator + "client.properties"));
+        final PropertiesConfiguration configuration = getSSLConfiguration(providerUrl);
+        TestUtils.writeConfiguration(configuration, persistDir + File.separator + "client.properties");
 
         dgiCLient = new AtlasClient(DGI_URL) {
             @Override
@@ -129,19 +82,13 @@ public class SSLHiveHookIT {
             }
         };
 
-        secureEmbeddedServer = new TestSecureEmbeddedServer(21443, "webapp/target/apache-atlas") {
+        secureEmbeddedServer = new TestSecureEmbeddedServer(21443, getWarPath()) {
             @Override
             public PropertiesConfiguration getConfiguration() {
                 return configuration;
             }
         };
-        WebAppContext webapp = new WebAppContext();
-        webapp.setContextPath("/");
-        webapp.setWar(System.getProperty("user.dir") + getWarPath());
-        secureEmbeddedServer.getServer().setHandler(webapp);
-
         secureEmbeddedServer.getServer().start();
-
     }
 
     @AfterClass
@@ -183,36 +130,8 @@ public class SSLHiveHookIT {
         }
     }
 
-    protected String getWarPath() {
-        return String.format("/../../webapp/target/atlas-webapp-%s",
-                System.getProperty("project.version"));
-    }
-
-    private HiveConf getHiveConf() {
-        HiveConf conf = new HiveConf();
-        conf.set(HiveMetaStoreBridge.ATLAS_ENDPOINT, DGI_URL);
-        return conf;
-    }
-
-    private void runCommand(String cmd) throws Exception {
-        ss.setCommandType(null);
-        driver.run(cmd);
-    }
-
     @Test
-    public void testCreateDatabase() throws Exception {
-        String dbName = "db" + RandomStringUtils.randomAlphanumeric(5).toLowerCase();
-        runCommand("create database " + dbName);
-
-        assertDatabaseIsRegistered(dbName);
-    }
-
-    private void assertDatabaseIsRegistered(String dbName) throws Exception {
-        assertInstanceIsRegistered(HiveDataTypes.HIVE_DB.getName(), "name", dbName);
-    }
-
-    private void assertInstanceIsRegistered(String typeName, String colName, String colValue) throws Exception {
-        JSONArray results = dgiCLient.rawSearch(typeName, colName, colValue);
-        Assert.assertEquals(results.length(), 1);
-    }
+    public void testService() throws Exception {
+        dgiCLient.listTypes();
+   }
 }
