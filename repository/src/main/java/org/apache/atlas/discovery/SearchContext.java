2b9ca749237d2cf78340d818f4415596e6a6eb98
Madhan Neethiraj <madhan@apache.org>
2017-07-18 00:25:36 -0700

    ATLAS-1961: Basic search improvement in use of Solr index for attribute filtering
    (cherry picked from commit 377fe19f023a4036f9fa1503dec6ea698fdb214b)
    (cherry picked from commit 17446d009a779a4a5bdbf2862b2c1934e694305f)
    
    Change-Id: I33e3602f86538bfe7e7812ac3d59d9e5830d5a41

--- repository/src/main/java/org/apache/atlas/discovery/SearchContext.java
+++ repository/src/main/java/org/apache/atlas/discovery/SearchContext.java
@@ -104,7 +104,7 @@ public class SearchContext {
     }
 
     public boolean needClassificationProcessor() {
-        return classificationType != null && (hasAttributeFilter(searchParameters.getTagFilters()) || entityType == null);
+        return classificationType != null;
     }
 
     public boolean needEntityProcessor() {
