0f3e742a4d3ca686fe1aab753279fac93d16dbba
Pierre Padovani <pierre.padovani@civitaslearning.com>
2018-03-01 16:06:00 -0600

    ATLAS-2470 - JanusGraph Cassandra support
    Signed-off-by: David Radley <david_radley@uk.ibm.com>
    (cherry picked from commit da8581e90aca74d1b3b04ee2efd5901b87b8d77d)
    
    Change-Id: Ib89c6b5bdda223de05fe9d623382fe1df665af5b

--- distro/src/bin/atlas_stop.py
+++ distro/src/bin/atlas_stop.py
@@ -68,6 +68,7 @@ def main():
 
     # stop solr
     if mc.is_solr_local(confdir):
+
         mc.run_solr(mc.solrBinDir(atlas_home), "stop", None, mc.solrPort(), None, True)
 
     if mc.is_cassandra_local(confdir):
