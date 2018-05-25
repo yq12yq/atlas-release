4326263fac97b7372075dcc98b822703e33a41a6
Graham Wallis <graham_wallis@uk.ibm.com>
2018-05-25 13:35:16 +0100

    ATLAS-2523 Add HomeId and allow GUIDs to be specified on creates
    (cherry picked from commit 72cc566a7f23bef86c00afb6c7ef136c1d01b424)
    
    Change-Id: Ie1b33a35a7956f3b98943bd748851bcb0bc4418a

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphRetriever.java
@@ -524,6 +524,8 @@ public final class EntityGraphRetriever {
         entity.setCreateTime(new Date(GraphHelper.getCreatedTime(entityVertex)));
         entity.setUpdateTime(new Date(GraphHelper.getModifiedTime(entityVertex)));
 
+        entity.setHomeId(GraphHelper.getHomeId(entityVertex));
+
         return entity;
     }
 
