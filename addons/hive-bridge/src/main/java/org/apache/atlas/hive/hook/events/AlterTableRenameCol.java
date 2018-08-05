apache/atlas

ATLAS-2757: fix for NPE in Hive hook in handling column-rename on temporary table

addons/hive-bridge/src/main/java/org/apache/atlas/hive/hook/events/AlterTableRenameCol.java
@@ -54,10 +54,17 @@ public class AlterTableRenameCol extends AlterTable {
             return null;
         }
 
-        List<HookNotification> ret = new ArrayList<>(super.getNotificationMessages());
+        List<HookNotification> baseMsgs = super.getNotificationMessages();
 
-        Table oldTable = getHiveContext().getInputs().iterator().next().getTable();
-        Table newTable = getHiveContext().getOutputs().iterator().next().getTable();
+        if (CollectionUtils.isEmpty(baseMsgs)) {
+            LOG.debug("Skipped processing of column-rename (on a temporary table?)");
+
+            return null;
+        }
+
+        List<HookNotification> ret      = new ArrayList<>(baseMsgs);
+        Table                  oldTable = getHiveContext().getInputs().iterator().next().getTable();
+        Table                  newTable = getHiveContext().getOutputs().iterator().next().getTable();
 
         newTable = getHive().getTable(newTable.getDbName(), newTable.getTableName());
 

yq12yq/atlas-release

(cherry picked from commit 75da039043f61c33330686e362de5c8c35996906)
(cherry picked from commit 218797aa491668a713bd7d446dcdbf392b3469c2)
Change-Id: I2dcadb776860faaa2c30205a409810af949fa164

addons/hive-bridge/src/main/java/org/apache/atlas/hive/hook/events/AlterTableRenameCol.java
@@ -55,10 +55,17 @@ public class AlterTableRenameCol extends AlterTable {
             return null;
         }
 
-        List<HookNotificationMessage> ret = new ArrayList<>(super.getNotificationMessages());
+        List<HookNotificationMessage> baseMsgs = super.getNotificationMessages();
 
-        Table oldTable = getHiveContext().getInputs().iterator().next().getTable();
-        Table newTable = getHiveContext().getOutputs().iterator().next().getTable();
+        if (CollectionUtils.isEmpty(baseMsgs)) {
+            LOG.debug("Skipped processing of column-rename (on a temporary table?)");
+
+            return null;
+        }
+
+        List<HookNotificationMessage> ret      = new ArrayList<>(baseMsgs);
+        Table                         oldTable = getHiveContext().getInputs().iterator().next().getTable();
+        Table                         newTable = getHiveContext().getOutputs().iterator().next().getTable();
 
         newTable = getHive().getTable(newTable.getDbName(), newTable.getTableName());
 