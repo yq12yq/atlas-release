apache/atlas

ATLAS-2823: updated hooks to support asynchronous notifications

addons/sqoop-bridge/src/main/java/org/apache/atlas/sqoop/hook/SqoopHook.java
@@ -52,8 +52,6 @@ import java.util.Date;
 public class SqoopHook extends SqoopJobDataPublisher {
     private static final Logger LOG = LoggerFactory.getLogger(SqoopHook.class);
 
-    public static final String CONF_PREFIX          = "atlas.hook.sqoop.";
-    public static final String HOOK_NUM_RETRIES     = CONF_PREFIX + "numRetries";
     public static final String ATLAS_CLUSTER_NAME   = "atlas.cluster.name";
     public static final String DEFAULT_CLUSTER_NAME = "primary";
 
@@ -71,8 +69,12 @@ public class SqoopHook extends SqoopJobDataPublisher {
     public static final String OUTPUTS        = "outputs";
     public static final String ATTRIBUTE_DB   = "db";
 
+    private static final AtlasHookImpl atlasHook;
+
     static {
         org.apache.hadoop.conf.Configuration.addDefaultResource("sqoop-site.xml");
+
+        atlasHook = new AtlasHookImpl();
     }
 
     @Override
@@ -95,7 +97,7 @@ public class SqoopHook extends SqoopJobDataPublisher {
 
             HookNotification message = new EntityCreateRequestV2(AtlasHook.getUser(), entities);
 
-            AtlasHook.notifyEntities(Collections.singletonList(message), atlasProperties.getInt(HOOK_NUM_RETRIES, 3));
+            atlasHook.sendNotification(message);
         } catch(Exception e) {
             LOG.error("SqoopHook.publish() failed", e);
 
@@ -224,5 +226,11 @@ public class SqoopHook extends SqoopJobDataPublisher {
         }
 
         return name.toString();
+    }
+
+    private static class AtlasHookImpl extends AtlasHook {
+        public void sendNotification(HookNotification notification) {
+            super.notifyEntities(Collections.singletonList(notification), null);
+        }
     }
 }

notification/src/test/java/org/apache/atlas/hook/AtlasHookTest.java
@@ -55,7 +55,7 @@ public class AtlasHookTest {
         List<HookNotification> hookNotifications = new ArrayList<>();
         doThrow(new NotificationException(new Exception())).when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotifications);
-        AtlasHook.notifyEntitiesInternal(hookNotifications, 0, notificationInterface, false,
+        AtlasHook.notifyEntitiesInternal(hookNotifications, 0, null, notificationInterface, false,
                 failedMessagesLogger);
         // if we've reached here, the method finished OK.
     }
@@ -69,7 +69,7 @@ public class AtlasHookTest {
             };
         doThrow(new NotificationException(new Exception())).when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotifications);
-        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, notificationInterface, false,
+        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, null, notificationInterface, false,
                 failedMessagesLogger);
 
         verify(notificationInterface, times(2)).
@@ -86,7 +86,7 @@ public class AtlasHookTest {
         doThrow(new NotificationException(new Exception(), Arrays.asList("test message")))
                 .when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotifications);
-        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, notificationInterface, true,
+        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, null, notificationInterface, true,
                 failedMessagesLogger);
 
         verify(failedMessagesLogger, times(1)).log("test message");
@@ -98,7 +98,7 @@ public class AtlasHookTest {
         doThrow(new NotificationException(new Exception(), Arrays.asList("test message")))
                 .when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotifications);
-        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, notificationInterface, false,
+        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, null, notificationInterface, false,
                 failedMessagesLogger);
 
         verifyZeroInteractions(failedMessagesLogger);
@@ -114,7 +114,7 @@ public class AtlasHookTest {
         doThrow(new NotificationException(new Exception(), Arrays.asList("test message1", "test message2")))
                 .when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotifications);
-        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, notificationInterface, true,
+        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, null, notificationInterface, true,
                 failedMessagesLogger);
 
         verify(failedMessagesLogger, times(1)).log("test message1");
@@ -126,7 +126,7 @@ public class AtlasHookTest {
         List<HookNotification> hookNotifications = new ArrayList<>();
         doThrow(new RuntimeException("test message")).when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotifications);
-        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, notificationInterface, true,
+        AtlasHook.notifyEntitiesInternal(hookNotifications, 2, null, notificationInterface, true,
                 failedMessagesLogger);
 
         verifyZeroInteractions(failedMessagesLogger);

yq12yq/atlas-release

(cherry picked from commit 52ef9e7f3962da2bf77a15ab1a1bcef8d47b2877)
(cherry picked from commit 9bdbb31842c93412ebfe27d98bd8dcbabe7d80f4)
(cherry picked from commit a90e506e5f4b192d2b33ebb6791985978687c9ad)
Change-Id: I85db9541c0f58c2ad25f1f846d9ee0992aae1bb1
(cherry picked from commit 8a801e89275c6ba6c676d443a8e75c072111a329)

addons/sqoop-bridge/src/main/java/org/apache/atlas/sqoop/hook/SqoopHook.java
@@ -52,8 +52,6 @@ import java.util.Date;
 public class SqoopHook extends SqoopJobDataPublisher {
     private static final Logger LOG = LoggerFactory.getLogger(SqoopHook.class);
 
-    public static final String CONF_PREFIX          = "atlas.hook.sqoop.";
-    public static final String HOOK_NUM_RETRIES     = CONF_PREFIX + "numRetries";
     public static final String ATLAS_CLUSTER_NAME   = "atlas.cluster.name";
     public static final String DEFAULT_CLUSTER_NAME = "primary";
 
@@ -70,8 +68,12 @@ public class SqoopHook extends SqoopJobDataPublisher {
     public static final String INPUTS         = "inputs";
     public static final String OUTPUTS        = "outputs";
 
+    private static final AtlasHookImpl atlasHook;
+
     static {
         org.apache.hadoop.conf.Configuration.addDefaultResource("sqoop-site.xml");
+
+        atlasHook = new AtlasHookImpl();
     }
 
     @Override
@@ -94,7 +96,7 @@ public class SqoopHook extends SqoopJobDataPublisher {
 
             HookNotificationMessage message = new EntityCreateRequestV2(AtlasHook.getUser(), entities);
 
-            AtlasHook.notifyEntities(Collections.singletonList(message), atlasProperties.getInt(HOOK_NUM_RETRIES, 3));
+            atlasHook.sendNotification(message);
         } catch(Exception e) {
             LOG.error("SqoopHook.publish() failed", e);
 
@@ -223,5 +225,11 @@ public class SqoopHook extends SqoopJobDataPublisher {
         }
 
         return name.toString();
+    }
+
+    private static class AtlasHookImpl extends AtlasHook {
+        public void sendNotification(HookNotificationMessage notification) {
+            super.notifyEntities(Collections.singletonList(notification), null);
+        }
     }
 }

notification/src/test/java/org/apache/atlas/hook/AtlasHookTest.java
@@ -54,7 +54,7 @@ public class AtlasHookTest {
         List<HookNotification.HookNotificationMessage> hookNotificationMessages = new ArrayList<>();
         doThrow(new NotificationException(new Exception())).when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotificationMessages);
-        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 0, notificationInterface, false,
+        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 0, null, notificationInterface, false,
                 failedMessagesLogger);
         // if we've reached here, the method finished OK.
     }
@@ -68,7 +68,7 @@ public class AtlasHookTest {
             };
         doThrow(new NotificationException(new Exception())).when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotificationMessages);
-        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, notificationInterface, false,
+        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, null, notificationInterface, false,
                 failedMessagesLogger);
 
         verify(notificationInterface, times(2)).
@@ -85,7 +85,7 @@ public class AtlasHookTest {
         doThrow(new NotificationException(new Exception(), Arrays.asList("test message")))
                 .when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotificationMessages);
-        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, notificationInterface, true,
+        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, null, notificationInterface, true,
                 failedMessagesLogger);
 
         verify(failedMessagesLogger, times(1)).log("test message");
@@ -97,7 +97,7 @@ public class AtlasHookTest {
         doThrow(new NotificationException(new Exception(), Arrays.asList("test message")))
                 .when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotificationMessages);
-        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, notificationInterface, false,
+        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, null, notificationInterface, false,
                 failedMessagesLogger);
 
         verifyZeroInteractions(failedMessagesLogger);
@@ -113,7 +113,7 @@ public class AtlasHookTest {
         doThrow(new NotificationException(new Exception(), Arrays.asList("test message1", "test message2")))
                 .when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotificationMessages);
-        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, notificationInterface, true,
+        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, null, notificationInterface, true,
                 failedMessagesLogger);
 
         verify(failedMessagesLogger, times(1)).log("test message1");
@@ -125,7 +125,7 @@ public class AtlasHookTest {
         List<HookNotification.HookNotificationMessage> hookNotificationMessages = new ArrayList<>();
         doThrow(new RuntimeException("test message")).when(notificationInterface)
                 .send(NotificationInterface.NotificationType.HOOK, hookNotificationMessages);
-        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, notificationInterface, true,
+        AtlasHook.notifyEntitiesInternal(hookNotificationMessages, 2, null, notificationInterface, true,
                 failedMessagesLogger);
 
         verifyZeroInteractions(failedMessagesLogger);